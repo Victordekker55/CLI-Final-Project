a2839241efb6084f110e565db8e95920fe894aac
Victordekker55 <victordekker55@gmail.com>
2022-04-22 12:24:43 +0200

    Meny in the middle with colour on the first row.

--- main.py
+++ main.py
@@ -1,28 +1,55 @@
 import curses
 import time
 
-from py import std
 
 
+MenyItems = ["Welcome to the bank", "Login", "Create account", "Exit"]
 def main(stdscr):
-    stdscr.clear()
     curses.curs_set(0)
-    stdscr.addstr(5,5, "Helloworld")
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    row_id = 0
+    menu(stdscr, row_id)
+    loop = True
+    while loop:
+        kboard = stdscr.getch()
+        stdscr.clear()
+
+        if kboard == curses.KEY_DOWN:
+            row_id += 1
+            stdscr.addstr(0,0,"Downkey")
+
+        stdscr.refresh()
+        time.sleep(5)
+
+
+myMenu = ["Welcome To The Bank", "Login", "Create Account", "Exit"]
+
+
+def menu(stdscr, row_id):
+    stdscr.clear()
+    h, w = stdscr.getmaxyx()
+    
+    for count, row in enumerate(myMenu):
+        x = w//2 - len(row)//2
+        y = h//2 - len(myMenu)//2 + count
+        if count == row_id:
+            stdscr.attron(curses.color_pair(1))
+            stdscr.addstr(y,x, row, curses.A_STANDOUT)
+            stdscr.attroff(curses.color_pair(1))
+        else:
+            stdscr.addstr(y,x,row)
+
+
+
+        
+
     stdscr.refresh()
+        
+
+
     time.sleep(5)
 
 
-    while True:
-
-        h,w = stdscr.getmaxyx()
-        val = stdscr.getch()
-        stdscr.clear()
-
-        if val == curses.KEY_UP:
-            stdscr.addstr((h//2), (w//2), "Down key ? ")
-            
-
-        stdscr.refresh()
 curses.wrapper(main)
 
 
