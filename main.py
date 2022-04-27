0e79d5fefad475eb6c341d91cbb4428f708602b8
Victordekker55 <victordekker55@gmail.com>
2022-04-27 13:56:06 +0200

    Added title with colour on it

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import curses
 import time
+import sys
 
 
+def print_slow(str):  #Humanized writing, action film vibe
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.008)
 
-MenyItems = ["Welcome to the bank", "Login", "Create account", "Exit"]
 def main(stdscr):
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
@@ -22,13 +27,25 @@ def main(stdscr):
         time.sleep(5)
 
 
-myMenu = ["Welcome To The Bank", "Login", "Create Account", "Exit"]
+myMenu = ["Login", "Create Account", "Exit"]
+myTitle = "Welcome to suedibank"
 
+
+myList = ["1"]
 
 def menu(stdscr, row_id):
     stdscr.clear()
+
     h, w = stdscr.getmaxyx()
+    max_y, max_x = stdscr.getmaxyx()
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
     
+    header = stdscr.subwin(1, (max_x//2)-(len(myTitle)//2))
+
+    header.attron(curses.color_pair(1))
+    header.addstr(myTitle)
+    header.refresh()
+
     for count, row in enumerate(myMenu):
         x = w//2 - len(row)//2
         y = h//2 - len(myMenu)//2 + count
@@ -36,7 +53,7 @@ def menu(stdscr, row_id):
             stdscr.attron(curses.color_pair(1))
             stdscr.addstr(y,x, row, curses.A_STANDOUT)
             stdscr.attroff(curses.color_pair(1))
-        else:
+        elif count != row_id:
             stdscr.addstr(y,x,row)
 
 
