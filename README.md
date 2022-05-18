cd64bc9571cc53b21ae525daa8d4ea55d35070c3
Victordekker55 <victordekker55@gmail.com>
2022-05-18 14:38:42 +0200

    V7

--- README.md
+++ README.md
@@ -1,7 +1,193 @@
 # CLI-Final-Project
 My Final Project in CLI. Making a CLI Menu interface
 
-In my code im using the curses libary
+In my program python is the used language with the downloadable libary curses. 
+The purpose is a neat menu with a login system.
 To install type in cmd promt or powershell "pip install windows-curses"
 
+## Description (Beskrivning)
 
+***Här bör du kort förklara vilket eller vilka språk programmet är skrivet i samt vad syftet med projektet är.***
+
+Denna README är skriven i markdown. Den är till för att ge ett exempel på vad en README kan innehålla och hur den kan se ut.
+
+## Technology/Languages/Built with (Teknologier/Språk/Byggt med - välj en)
+
+***Här kan du beskriva vilka språk som driver vilka delar av projektet, exempelvis att du skrivit frontend med JavaScript och Python för backend. Det kan även vara nyttigt att berätta vilka ramverk eller bibliotek du använt här för de olika delarna. För exemplena givna är React (för JS) och Flask (Python) bra att nämna. Utelämna add-ons eller plugins, lämna dessa till Acknowledgements.***
+I have used python for all the parts in the code. Frontend aswell as backend was built with the help of the libary windows curses. 
+
+- Python
+
+
+## Requirements
+
+***Vad krävs för att köra ditt program? Lista bara kraven.***
+
+- Python 3.7+
+
+- Time module
+- Curses module
+- Random module
+
+## Installation
+
+
+
+1. Clone git repo
+```cmd
+    git clone https://github.com/Victordekker55/CLI-Final-Project
+```
+2. Installera Curses
+```cmd
+pip install windows.curses
+```
+
+
+Exempel 2:
+
+This program is tested with python 3.7+. To download python visit:
+ (https://www.python.org/downloads/)
+
+Programmet kräver även att biblioteket Flask samt pygame är installerade. För att installera krävs python 3.7+ installerat, följt av att följande kodrader skrivs i terminalen: 
+
+```cmd
+pip install windows.curses
+```
+
+## Code conventions (Kodkonvention)
+
+För att det ska fungera måste resource.txt ligga i samma mapp som main. 
+
+## Standards
+-Pep8
+-Docstrings
+
+## Hur det fungerar (Usage)
+
+***Använd detta utrymme för att visa användbara exempel av hur projektet kan användas. Skärmdumpar, kodexempel och demos passar in här. Du kan också länka till fler resurser, exempelvis en dokumentation.***
+
+## Example 
+
+# Main menu:
+https://github.com/Victordekker55/CLI-Final-Project/issues/1
+
+
+## To do/Roadmap
+
+
+
+- [x] Påbörja exempelreadme
+- [X] Påbörja filhantering
+- [] Kom på bättre exempel
+- [X] Nagivera inom menyn
+- [] Ytterligare språk
+    - [] Svenska
+    - [X] Engelska
+
+
+## Changelog
+
+***Det kan vara rimligt att inkludera vad som har förändrats genom de olika iterationerna som ditt projekt gått igenom. Detta kan antingen göras i din README eller så kan du inkludera en CHANGELOG.md.***
+
+***I changelogen ska varje rubrik vara en version. Under varje version bör du inkludera vad du lagt till eller ändrat på (added or changed) under en rubrik samt vad du tagit bort (removed) under en annan. Exempel: ***
+
+### Version 1
+
+### Added
+
+- Rendered a str on the cmd screen
+
+## Removed
+
+- None
+
+### Version 2
+
+## Added
+- Can now show color
+
+## Removed
+
+-None
+
+### Version 3
+
+## Added
+
+- Began with basic menu without color. 
+
+## Removed
+
+- None
+
+### Version 4
+
+## Added
+
+- Can now take key inputs from the cmd prompt and show what keys pressed
+
+## Removed
+
+- Removed old code that was for used for learning purposes
+
+### Version 5
+
+## Added
+
+- Menu now shows color on which row is selected
+- It is now possible to choose options in menu
+- Added cool humanized writing
+- Can show txt from a file
+
+## Removed
+
+- None
+
+### Version 6
+
+## Added
+
+- Added login page
+- Can now create an account and it will save into a txt file
+- Can now login with that account and come to the login page
+- Can change an accounts money and rewriting it into the txt so it changes. 
+
+## Removed
+
+-None
+
+## Att bidra (Contribution)
+
+***Inom programmeringsvärlden är det ofta populärt att man vill utveckla andras projekt och bidra till förbättring. För att underlätta detta är det bra om det i READMEn förklaras om det är tillåtet, och om det är det hur en går till väga för att kunna göra det. Detta avsnitt skulle se ut som följande:*** 
+
+Då bedömning ännu ej är gjord på uppgiften så tillåts inga pull requests. Så fort bedömning är gjord kommer detta tillåtas.  
+
+Vid större förändringar önskar jag att en issue öppnas för diskussion om vad som ska förändras.
+
+## Licens (License)
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Contact (Kontakt)
+
+***Skriv här hur du blir kontaktad ifall det finns frågor om projektet***
+
+Victor Dekker - victordekker55@gmail.com
+
+Projektlänk: https://github.com/ditt_anv/reponamn
+
+## Erkännanden (Acknowledgments)
+
+***Här kan du lista resurser eller personer som har hjälpt dig med projektet. Det kan vara länkar till tutorials eller dokumentation, eller bara någon annans profil som du vill uppmärksamma. Har du inget som behöver tas här så kan du strunta i rubriken. ***
+
+- Techwithtim:
+https://www.youtube.com/watch?v=H-DCpFRQ2dY&t=421s
+https://www.youtube.com/watch?v=elfqLbiLAT4
+https://www.youtube.com/watch?v=Db4oc8qc9RU
+His github : https://github.com/techwithtim
+
+- Stackoverflow:
+https://stackoverflow.com/questions/21784625/how-to-input-a-word-in-ncurses-screen
+No github found
+
+-
--- main.py
+++ main.py
@@ -12,6 +12,9 @@ subtitle = "Beta version"
 footer = "Written by Victor Dekker                           Press q to exit                        Press a random key to refresh"
 myList = ["1"]
 
+with open("resource.txt", "r") as file:
+    data = file.read()
+
 
 
 #Cool humanized writing
@@ -27,30 +30,47 @@ def run_slow(stdscr, the_string):
 
 
 def login_page(stdscr, username):
+    """_summary_
+
+    Args:
+        stdscr (Terminal value): Enables the function to control the cmd prompt screen
+        username (str): Takes the username from the login input and welcomes the user that logged in 
+    
+
+    Returns:
+        It returns the login page
+    """
+    #Clear the screen and dont show the inputs and enable
     stdscr.clear()
     curses.noecho()
     curses.cbreak()
     stdscr.keypad(True)
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    #Get max height, width of screen
     h,w = stdscr.getmaxyx()
+    #Centering calculation
     max_y = h//2
     max_x = w//2
     
-    
+    #Calcuting how much should be filled in the footer with white color
     footerfiller = " " * (w-len(footer)-1)
-
+    #Enabling color for the footer
     stdscr.attron(curses.color_pair(1))
+    #Rendering footer
     stdscr.addstr((h-1), 0, footer)
     stdscr.addstr((h-1), len(footer), footerfiller)
+    #Disabling color
     stdscr.attroff(curses.color_pair(1))
     stdscr.refresh()
     moneycount = 0
 
     while True:
+        #Get inputs from screen
         mainkey = stdscr.getkey()
         
         
-        moneytxt = "Money added"
+        txtadd = "Money added"
+        #Make random cordinates
         ran_y = random.randrange(0,h)
         ran_x = random.randrange(0,w)
 
@@ -58,9 +78,10 @@ def login_page(stdscr, username):
             for line in f.readlines():
                 attr = line.split("/")
                 money = attr[2]
-                
+                money = int(money)
+                moneytxt = f"{money}btc"
 
-                stdscr.addstr(1,1,f"Nice to have you here {username}, you have {money} btc in your wallet press + to add btc")
+                stdscr.addstr(1,1,f"Nice to have you here {username}, you have {moneytxt} in your wallet press + to add btc")
                 stdscr.addstr(max_y, max_x, f"BTC Wallet:{money}")
                 stdscr.refresh()
 
@@ -77,18 +98,19 @@ def login_page(stdscr, username):
             with open("resource.txt", "w") as file:
                 file.write(changed)
 
-            stdscr.addstr(ran_y, ran_x, moneytxt)
+            stdscr.addstr(ran_y, ran_x, txtadd)
             
 
         if mainkey == "q":
             return main(stdscr)
 
         stdscr.refresh()
-
-
-
-
 def main(stdscr):
+    """_summary_
+
+    Args:
+        stdscr (Terminal value): Enables the function to control the cmd prompt screen
+    """
     #creating diffrent color sets
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
@@ -100,6 +122,7 @@ def main(stdscr):
     loop = True
 
     while loop:
+        
         #rendering menu
         menu(stdscr, row_id)
         #getting key inputs from the screen
@@ -118,40 +141,55 @@ def main(stdscr):
         if key =="q":
             loop = False
         
-        #checking if 
+        #checking if a key is pressed and row id is on log in 
         if key == ("a" or "d") and row_id == 0:
+        
+
             stdscr.clear()
             txt_list = []
             user_list = []
             with open("resource.txt", "r", encoding="utf8") as r:
                 for line in r.readlines():
+                    #Getting username and password with line.split
                     attr = line.split("/")
                     user_name = attr[0]
                     user_password = attr[1]
+                    #Rendering the user and password for testing purposes, so you know if it works. Can be removed
+                    stdscr.addstr(19,((w-w)+1), "Beta version, for test purposes, can be removed")
 
                     stdscr.addstr(20,((w-w)+1), f"user:{user_name}")
                     stdscr.addstr(21,((w-w)+1), f"pass:{user_password}")
 
                     stdscr.refresh()
+            #Using humanized writing function, takes in a str
             run_slow(stdscr, "Login Page - Type in user/pass form, CTRL+G to sumbit")
+            # Creating first a window a certain size, then making it a textbox
             win2 = curses.newwin(2,15,3,3)
             box2 = Textbox(win2)            
             stdscr.refresh()
+            #Users can type in the textbox until CTRL+ G is pressed with .edit()
             box2.edit()
+            #Takes the content of the textbox
             txt_login = box2.gather()
+            #Putting the content into a list
             txt_list.append(txt_login)
+            # Using lamba to replace \n in the list with blank space for the login system to work.
             txt_list = list(map(lambda x: x.replace("\n", ""), txt_list))
-
+            
             stdscr.refresh()
+
             with open("resource.txt", "r") as f:
+                #Readlines to read each line
                 for line in f.readlines():
                     pos = line.split("/")
+                    #Separate user and pass with line.split
                     user_login = pos[0]
                     pass_login = pos[1]
+                    #Adding blank to pass because the input from the textbox adds an blank
                     pass_login = pass_login + " "
-                    
+                #Putting it in a list
                 user_list.append(f"{user_login}/{pass_login}")
-
+            # checking if the input list equals to 
             if txt_list == user_list:
                 
                 stdscr.addstr(10,9,"logging in...")
@@ -161,43 +199,55 @@ def main(stdscr):
                 
                         
             else:
-                stdscr.addstr(8,9, f"input:{txt_list}")
-
-                stdscr.addstr(9,9,(f"pass:{user_list}"))
-           
-        if key == ("a" or "d") and row_id == 1:
+                stdscr.clear()
+                stdscr.addstr(8,9,"Wrong credentials, shutting down")
+                stdscr.refresh()
+                time.sleep(3)
+                loop = False
+        if key == ("a" or "d") and row_id == 1: 
             stdscr.clear()
-            key2 = stdscr.getch()
-            
+            #Using humanized funcion, takes str            
             run_slow(stdscr, "Create password - Type in user/pass form, CTRL+G to save")
+            #Creating a subwindow as above
             win = curses.newwin(2,15,3,3)
+            #Creating a textbox in that subwindow as above
             box = Textbox(win)            
             stdscr.refresh()
             box.edit()
+            #Getting the content from the textbox
             txt = box.gather()
+            #Organizing the content
             txt = txt.strip().replace("\n", "")
             txt = txt + "/0"
             stdscr.addstr(10,10, txt)
             stdscr.refresh()
+            #Writing the account into a textfile
             with open("resource.txt", "w", encoding="utf8") as file:
-                for i in txt:                    
+                for i in txt:
+                                
                     file.write(i)
                 file.write("\n")
                 stdscr.addstr(12,12, "Account has been created")
                 stdscr.refresh()
                 time.sleep(2)
             stdscr.clear()    
+        
             
-                    
+            
 def menu(stdscr, row_id):
-    
+    """_summary_
+
+    Args:
+        stdscr (terminal): stdscr is from the module curses, its used to controll the cmd screen
+        row_id (int): row_id takes in a number and starts the menu on that number, it should be 0 so it starts on the first item
+    """
+    # Get max with height
     h, w = stdscr.getmaxyx()
-    max_y, max_x = stdscr.getmaxyx()
     #filler for footer
     footerfiller = " " * (w-len(footer)-1)
     #creating header & subtitle
-    header = stdscr.subwin(1, (max_x//2)-(len(myTitle)//2))
-    subheader = stdscr.subwin(2, (max_x//2)-(len(subtitle)//2))
+    header = stdscr.subwin(1, (w//2)-(len(myTitle)//2))
+    subheader = stdscr.subwin(2, (w//2)-(len(subtitle)//2))
     #rendering header
     header.attron(curses.color_pair(3))
     header.addstr(myTitle)
@@ -217,6 +267,7 @@ def menu(stdscr, row_id):
 
     stdscr.attroff(curses.color_pair(1))
 
+    #Show login credentials in main screen, uncomment for testing purposes
 
     # with open("resource.txt", "r", encoding="utf8") as r:
     #     for line in r.readlines():
@@ -232,10 +283,10 @@ def menu(stdscr, row_id):
     
     #creating menu with enumerate so u get 2 values, count is for which count in the list its on and row is the str in the list.
     for count, row in enumerate(myMenu):
-        #centering menu
+        #centering calculations
         x = w//2 - len(row)//2
         y = h//2 - len(myMenu) + count
-        
+        #Rendering menu 
         if count == row_id:
             stdscr.attron(curses.color_pair(1))
             stdscr.addstr(y,x, row, curses.A_STANDOUT)
@@ -245,8 +296,9 @@ def menu(stdscr, row_id):
     # refreshing screen
     stdscr.refresh()
         
+if __name__ == "__main__":
 
-curses.wrapper(main)
+    curses.wrapper(main)
 
 
 
